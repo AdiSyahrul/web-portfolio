8308176b98b31b46495b2b150ab2a3c857516e24
AdiSyahrul <adisyahrul27@gmail.com>
2025-01-10 09:30:04 +0700

    configure gh-pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import react from '@tailwindcss/typography'
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [react()],
+  base: '/web-portfolio/',
 }
 
